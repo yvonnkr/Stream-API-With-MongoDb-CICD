6c602835b18ea57b3cf6a50ab3897aa59fce047b
yvonnkr <yvonnkr86@gmail.com>
2024-04-20 02:13:55 +0100

    Initial commit

---
+++ src/main/java/com/yvolabs/streamapi/StreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.streamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yvolabs/streamapi/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
